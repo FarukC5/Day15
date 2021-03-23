647626c136c60be25abbe090f3411a0596bdb908
FarukC5 <faruk_cajic@yahoo.com>
2021-03-23 10:49:40 +0100

    Added

---
+++ ex04/src/index.js
@@ -0,0 +1,35 @@
+const milili = [10, 25, 4]; // DO NOT change this line
+
+function myArr(mili) {
+  "use strict";
+  // Only change code below this line
+ // using mili = [4, 10 ,25] would be invalid
+  mili[0] = 4;
+  mili[1] = 10;
+  mili[2] = 25;
+  // Only change code above this line
+  return mili;
+}
+console.log(myArr(milili));
+
+module.exports = myArr;
+
+/*
+
+const milili = [10, 25, 4]; // DO NOT change this line
+
+function myArr(mili) {
+  "use strict";
+// Only change code below this line
+milili = [...mili];
+mili[0] = 4;
+mili[1] = 10;
+mili[2] = 25;
+
+// Only change code above this line
+    return mili;
+}
+console.log(myArr(milili));
+module.exports = myArr;
+
+*/
---
+++ ex06/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+const arena = () => {
+  "use strict";
+  return  new Date(); // sa new ili ne
+};
+// Only change code above
+console.log(arena());
+module.exports = arena;
